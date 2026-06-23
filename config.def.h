4cf28a0f7bd7b86b7d01f2602473b05e7f27e098
Angelo Gambara <angelo@angelogambara.dev>
2026-06-23 15:52:38 +0200

    fix(menucmd): add quotes to font size

--- config.def.h
+++ config.def.h
@@ -146,7 +146,7 @@ static const char *menucmd[] = {
 	"bemenu-run",
 	"-m",  "-2",
 	"-l",  "10"
-	"--fn", "Hack 14",
+	"--fn", "Hack", "14",
 	"--tb", "#d79921",
 	"--tf", "#1d2021",
 	"--hb", "#d79921",
